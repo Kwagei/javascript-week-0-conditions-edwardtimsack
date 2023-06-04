a0ab5e89c2323fd74de7d4b9158a459074a1fc0a
edwardtimsack <edwardtimothysackie@gmail.com>
2023-06-04 18:24:30 +0000

    completed Greatest COmmon Divisor

---
+++ solution1.js
@@ -0,0 +1,10 @@
+var firstNumber = 28;
+var secondNumber = 56;
+var gcd = 2;
+
+if(firstNumber % gcd === 0 && secondNumber % gcd === 0){
+		console.log(`The Greatest Common Divisor for 28 and 56 is ${gcd}`)
+}
+else{
+		console.log("The is no Greatest Common Divisor for 28 and 56")
+}
